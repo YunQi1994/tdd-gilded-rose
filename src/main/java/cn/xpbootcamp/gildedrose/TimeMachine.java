12b7044c962ea20d6a29c047b30c16dfc0414336
YunQi1994 <317458884@qq.com>
2020-05-13 01:06:33 +0800

    add test to verify SellIn changes of normal product; pass compile

---
+++ src/main/java/cn/xpbootcamp/gildedrose/TimeMachine.java
@@ -0,0 +1,11 @@
+package cn.xpbootcamp.gildedrose;
+
+public class TimeMachine {
+    public TimeMachine(Product product) {
+
+    }
+
+    public Product productTimeTravel(int i) {
+        return null;
+    }
+}
