a80e68694c3940eba9dd3e4c2d03ddd3d6a2c51c
YunQi1994 <317458884@qq.com>
2020-05-13 20:55:27 +0800

    add test to verify SellIn and quality decrease by 1 of normal product ; pass test

--- src/main/java/cn/xpbootcamp/gildedrose/Product.java
+++ src/main/java/cn/xpbootcamp/gildedrose/Product.java
@@ -10,12 +10,21 @@ import lombok.Setter;
 @Setter
 public class Product {
 
-    @NonNull private String name;
-    @NonNull private int sellIn;
-    @NonNull private int quality;
+    @NonNull
+    private int sellIn;
+    @NonNull
+    private int quality;
 
     public void travelOneDay() {
         sellIn--;
         quality--;
+    }
+
+    public void travel(int days) {
+
+
+        sellIn = sellIn - 1;
+        quality = quality - 1;
+
     }
 }
--- src/test/java/cn/xpbootcamp/gildedrose/ProductTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/ProductTest.java
@@ -9,27 +9,37 @@ public class ProductTest {
 
     private String name;
 
-    @Before
-    public void setTestProduct(){
-        name = "Iron Sword";
-    }
-
     @Test
     public void should_return_product_info_given_a_product_is_created_when_check_product_info() {
-        Product product = new Product(name, 20, 10);
+        Product product = new Product( 20, 10);
         assertThat(product.getQuality()).isEqualTo(10);
         assertThat(product.getSellIn()).isEqualTo(20);
-        assertThat(product.getName()).isEqualTo(name);
     }
 
     @Test
-    public void should_decrease_SellIn_by_5_given_a_normal_on_date_product_when_has_existed_5_days() {
-        Product product= new Product(name, 20,10);
+    public void should_decrease_SellIn_by_1_given_a_on_date_product_when_has_existed_1_day() {
+        Product product= new Product(20,10);
         product.travelOneDay();
 
         assertThat(product.getQuality()).isEqualTo(9);
         assertThat(product.getSellIn()).isEqualTo(19);
-        assertThat(product.getName()).isEqualTo("Iron Sword");
     }
+
+    @Test
+    public void should_decrease_SellIn_by_5_given_a_on_date_product_when_travel (){
+        Product product = new Product(20, 10);
+        product.travel(1);
+
+        assertThat(product.getSellIn()).isEqualTo(19);
+        assertThat(product.getQuality()).isEqualTo(9);
+    }
+
+//    @Test
+//    public void should_decrease_by_2_points_given_a_out_of_date_product_when_travel (){
+//        Product product = new Product(1, 20);
+//        product.travel(1);
+//        assertThat(product.getQuality()).isEqualTo(17);
+//
+//    }
 
 }
